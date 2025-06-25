f412a324b1d94b686a6fecddc624c5abfef4a839
Moeka Nawata <nawata@marimo-gt.co.jp>
2025-06-25 05:08:32 +0000

    .restructure1

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,13 +1,22 @@
+// アプリの動きを管理、初期化を行う
+// manifest.jsonはアプリの構造を設定、定義する
 sap.ui.define([
     "sap/ui/core/UIComponent"
 ],
+
     function (UIComponent) {
         "use strict";
 
         return UIComponent.extend("sap.training.exc.Component", {
+            // manifest.jsonから設定を読み取る
+            metadata: {
+                manifest: "json"
+            },
 
-
-
+            init: function () {
+                // call the base component's init function　初期化処理
+                UIComponent.prototype.init.apply(this, arguments);
+            }
 
         });
     }

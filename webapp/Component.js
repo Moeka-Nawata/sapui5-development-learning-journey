a9193d5e5a339b91675bcb17dc27c0cc7f26163d
Moeka Nawata <nawata@marimo-gt.co.jp>
2025-06-25 07:11:55 +0000

    .adjustmentOfContents

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,10 +1,11 @@
 // アプリの動きを管理、初期化を行う
 // manifest.jsonはアプリの構造を設定、定義する
 sap.ui.define([
-    "sap/ui/core/UIComponent"
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device"
 ],
 
-    function (UIComponent) {
+    function (UIComponent, Device) {
         "use strict";
 
         return UIComponent.extend("sap.training.exc.Component", {
@@ -16,6 +17,17 @@ sap.ui.define([
             init: function () {
                 // call the base component's init function　初期化処理
                 UIComponent.prototype.init.apply(this, arguments);
+            },
+
+            getContentDensityClass: function () {
+                if(!this._sContentDensityClass) {
+                    if(Device.support.touch) {
+                        this._sContentDensityClass = "sapUiSizeCozy";
+                    }else{
+                        this._sContentDensityClass = "sappUiSizeCompact";
+                    }
+                }
+                return this._sContentDensityClass;
             }
 
         });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -7,6 +7,10 @@ sap.ui.define([
 
         return Controller.extend("sap.training.exc.controller.App", {
 
+            onInit: function () {
+                this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+            }
+
         });
     }
 );
